8bb0a54f5d3a163a6bdebaca45802994eff1430b
Ксения Шлёнская <k.shlenskaya@duotek.ru>
2025-08-02 13:00:41 +0300

    Add BurgerIngredientsSection component

--- src/components/burgerIngredients/BurgerIngredients.jsx
+++ src/components/burgerIngredients/BurgerIngredients.jsx
@@ -1,8 +1,8 @@
 import './burger-ingredients.css';
 
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import BurgerIngredientsSection from './burgerIngredientsSection/BurgerIngredientsSection';
 
 import { useState } from 'react';
 import { useEffect, useRef } from 'react';
@@ -44,17 +44,13 @@ const BurgerIngredients = ({ className, ingredients, selectedIngredients, onSele
         return () => observer.disconnect();
     }, []);
 
-    const getIngredientCount = (ingredient) => {
-        return selectedIngredients.filter(item => item._id === ingredient._id).length;
-    };
-
     const handleTabClick = (tab) => {
         setActiveTab(tab);
         const section = sectionsRef.current[tab];
         section?.scrollIntoView({ behavior: 'smooth', block: 'start' });
     };
 
-    const handleCardClick = (ingredient) => {
+    const handleIngredientClick = (ingredient) => {
         onSelectIngredient(ingredient);
     };
 
@@ -76,73 +72,15 @@ const BurgerIngredients = ({ className, ingredients, selectedIngredients, onSele
             </div>
             <div className="burger-ingredients__scroll-wrap">
                 <div className="burger-ingredients__scroll">
-                    <section className="burger-ingredients__section pt-10" ref={el => sectionsRef.current['Булки'] = el}>
-                        <h2 className="text text_type_main-medium">
-                            Булки
-                        </h2>
-                        <ul className="burger-ingredients__grid mt-6">
-                            {buns.map((bun, index) => (
-                                <li key={index} className="burger-ingredients__card" onClick={() => handleCardClick(bun)}>
-                                    {getIngredientCount(bun) > 0 &&
-                                        <Counter count={getIngredientCount(bun)} size="default" extraClass="m-1" />
-                                    }
-                                    <img className="burger-ingredients__card-image" alt={bun.name} src={bun.image} />
-                                    <p className="burger-ingredients__sup-title text text_type_digits-default mt-1">
-                                        <span className="mr-2">{bun.price}</span>
-                                        <CurrencyIcon type="primary" />
-                                    </p>
-                                    <h3 className="burger-ingredients__title text text_type_main-default mt-2">
-                                        {bun.name}
-                                    </h3>
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
-                    <section className="burger-ingredients__section pt-10" ref={el => sectionsRef.current['Соусы'] = el}>
-                        <h2 className="text text_type_main-medium">
-                            Соусы
-                        </h2>
-                        <ul className="burger-ingredients__grid mt-6">
-                            {sauces.map((sauce, index) => (
-                                <li key={index} className="burger-ingredients__card" onClick={() => handleCardClick(sauce)}>
-                                    {getIngredientCount(sauce) > 0 &&
-                                        <Counter count={getIngredientCount(sauce)} size="default" extraClass="m-1" />
-                                    }
-                                    <img className="burger-ingredients__card-image" alt={sauce.name} src={sauce.image} />
-                                    <p className="burger-ingredients__sup-title text text_type_digits-default mt-1">
-                                        <span className="mr-2">{sauce.price}</span>
-                                        <CurrencyIcon type="primary" />
-                                    </p>
-                                    <h3 className="burger-ingredients__title text text_type_main-default mt-2">
-                                        {sauce.name}
-                                    </h3>
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
-                    <section className="burger-ingredients__section pt-10" ref={el => sectionsRef.current['Начинки'] = el}>
-                        <h2 className="text text_type_main-medium">
-                            Начинки
-                        </h2>
-                        <ul className="burger-ingredients__grid mt-6">
-                            {maines.map((main, index) => (
-                                <li key={index} className="burger-ingredients__card" onClick={() => handleCardClick(main)}>
-                                    {
-                                        getIngredientCount(main) > 0 &&
-                                        <Counter count={getIngredientCount(main)} size="default" extraClass="m-1" />
-                                    }
-                                    <img className="burger-ingredients__card-image" alt={main.name} src={main.image} />
-                                    <p className="burger-ingredients__sup-title text text_type_digits-default mt-1">
-                                        <span className="mr-2">{main.price}</span>
-                                        <CurrencyIcon type="primary" />
-                                    </p>
-                                    <h3 className="burger-ingredients__title text text_type_main-default mt-2">
-                                        {main.name}
-                                    </h3>
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
+                    <div ref={el => sectionsRef.current['Булки'] = el}>
+                        <BurgerIngredientsSection extraClass="pt-10" title="Булки" ingredients={buns} selectedIngredients={selectedIngredients} onIngredientClick={handleIngredientClick} />
+                    </div>
+                    <div ref={el => sectionsRef.current['Соусы'] = el}>
+                        <BurgerIngredientsSection extraClass="pt-10" title="Соусы" ingredients={sauces} selectedIngredients={selectedIngredients} onIngredientClick={handleIngredientClick} />
+                    </div>
+                    <div ref={el => sectionsRef.current['Начинки'] = el}>
+                        <BurgerIngredientsSection extraClass="pt-10" title="Начинки" ingredients={maines} selectedIngredients={selectedIngredients} onIngredientClick={handleIngredientClick} />
+                    </div>ч
                 </div>
             </div>
         </div>
